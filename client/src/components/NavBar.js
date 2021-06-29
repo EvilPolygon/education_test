bc2c64ee1a1933a7ddd86241e626e62782c017dd
EvilPolygon <59900883+EvilPolygon@users.noreply.github.com>
2021-06-29 23:34:32 +0500

    end of the 29.06

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,16 +11,16 @@ export const NavBar = () => {
     const logoutHandler = event => {
         event.preventDefault()
         auth.logout()
-        history.pushState('/')
+        history.push('/')
     }
 
     return (
         <nav>
-            <div className="nav-wrapper">
-                <a href="/" className="brand-logo">Сокращение ссылок</a>
+            <div className="nav-wrapper blue darken-1" style ={{padding: '0 2rem'}}>
+                <span href="/" className="brand-logo">Сокращение ссылок</span>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><NavLink to = "/create">Создать</NavLink></li>
-                    <li><NavLink to = "/create">Создать</NavLink></li>
+                    <li><NavLink to = "/links">Ссылки</NavLink></li>
                     <li><a href ="/" onClick = {logoutHandler}>Выйти</a></li>
                 </ul>
             </div>
