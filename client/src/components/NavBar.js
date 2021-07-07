c1bceb59fe74ed86f34a4df65cf4437ef8429622
EvilPolygon <59900883+EvilPolygon@users.noreply.github.com>
2021-07-08 02:04:47 +0500

    end of 7.07, auth fail

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -17,7 +17,7 @@ export const NavBar = () => {
     return (
         <nav>
             <div className="nav-wrapper blue darken-1" style ={{padding: '0 2rem'}}>
-                <span href="/" className="brand-logo">Сокращение ссылок</span>
+                <span className="brand-logo">Сокращение ссылок</span>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><NavLink to = "/create">Создать</NavLink></li>
                     <li><NavLink to = "/links">Ссылки</NavLink></li>
--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,8 +1,48 @@
+import React, {useState, useEffect, useContext} from 'react'
+import { AuthContext } from '../context/auth.context'
+import {useHttp} from '../hooks/http.hook'
 
 export const CreatePage = () => {
-    return(
-        <div>
-            <h1> Create page</h1>
+
+    const {request} = useHttp()
+
+    const [link, setLink] = useState('')
+
+    const auth = useContext(AuthContext)
+
+    useEffect( () => {
+        window.M.updateTextFields()
+    }, [])
+
+    const pressHandler = async event => {
+        if (event.key === 'Enter'){
+            try {
+                
+                const data = await request('/api/link/generate', 'POST', {from: link}, {
+                    Authorization: 'Bearer ${auth.token}'  // здесь эта срань не работает. надо искать другой способ подтверждения авторизации
+                })
+                console.log(data)
+
+            } catch (e) {         
+            }
+        }
+    }
+
+    return (
+        <div className="row">
+            <div className=" col s8 offset-s2" style={{ paddingTop: '2rem' }}>
+                <div className="input-field">
+                    <input
+                        placeholder="Вставьте ссылку"
+                        id="link"
+                        type="text"
+                        value = {link}
+                        onChange={e => setLink(e.target.value)}
+                        onKeyPress = {pressHandler}
+                    />
+                    <label htmlFor="link">Введите ссылку</label>
+                </div>
+            </div>
         </div>
     )
 }
